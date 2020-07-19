b7c985854a3a542e70c247b2e520796a1a6a3553
vtr222 <56329969+vtr222@users.noreply.github.com>
2020-07-18 23:19:25 -0300

    Add files via upload

--- Prototipo_inicial_v0/teste.js
+++ Prototipo_inicial_v0/teste.js
@@ -1,14 +1,60 @@
+var getDuration = {
+    '/w': 4,
+    '/h': 2,
+    '/q': 1,
+    '/8': 0.5,
+}
+
 let vue = new Vue({
-    el:'#music-container',
+    el: '#music-container',
     data: {
-        clef:'treble',
-        timeSignature:'4/4',
-        staves: [  
-                {notes:[{string:'C#5/h', duration:2},{string:'B4/h/r',duration:2}]},
-                {notes:[{string:'C#5/h', duration:2},{string:'B4/h/r',duration:2}]}, 
-        ],
+        clef: 'treble',
+        timeSignature: '4/4',
+        defaultOctave: '/4',
+        defaultMeasures: 0,
+        currentFigure: '/q',
+        restActive: false,
+        noteList: ['D4/8'],
         staveWidth: 280,
-        measuresPerLine: 5
+        measuresPerLine: 5,  
+    },
+    computed: {
+        beats(){return  parseInt(this.timeSignature.slice(this.timeSignature.length - 1)) },
+        staves() {
+            let stavesArr = []
+            let currentArr = []
+            let timeSum = 0
+            this.noteList.forEach((note,i) => {
+                let noteDuration = getDuration[note.slice(-2)]
+                if(timeSum + noteDuration < this.beats) {
+                    currentArr.push({string:note})
+                    timeSum += noteDuration
+                }
+                else if(timeSum + noteDuration === this.beats) {
+                    currentArr.push({string:note})
+                    stavesArr.push({notes: currentArr})   
+                    currentArr = []    
+                    timeSum = 0
+                }
+                else {
+                    console.log('s')
+                    
+                    fillRest(currentArr, this.beats - timeSum)
+                    stavesArr.push({notes: currentArr})
+                    currentArr = []
+                    currentArr.push({string:note})   
+                    timeSum = noteDuration
+                }
+            })
+
+            if(timeSum!== 0) {
+                fillRest(currentArr, this.beats - timeSum)
+                stavesArr.push({notes: currentArr})
+                currentArr = []
+            }
+
+            return stavesArr
+        }
     },
 
     watch: {
@@ -20,6 +66,29 @@ let vue = new Vue({
 const VF = Vex.Flow;
 var vf, score, system
 
+function fillRest(stave, remaining) {
+
+    
+    let order = Object.keys(getDuration)
+    let values = Object.values(getDuration)
+ 
+    while(remaining > 0) {
+        if(remaining - values[0] >= 0) {
+            remaining -= values[0]
+            stave.push({string:restString(order[0])})
+        }
+        else {
+            order.shift()
+            values.shift()
+        }
+    }
+    
+}
+
+function restString(time) {
+    return `B4${time}/r`
+}
+
 function setupVexFlow() {
     x = 0
     y = 80
@@ -27,47 +96,40 @@ function setupVexFlow() {
     let div = document.createElement('div')
     div.id = 'vf'
     document.querySelector('body').appendChild(div)
-    vf = new VF.Factory({renderer: {elementId: 'vf', height: 900, width:1600}});
+    vf = new VF.Factory({ renderer: { elementId: 'vf', height: 900, width: 1600 } });
     score = vf.EasyScore();
-    
+
 }
 
 function drawSheet() {
     setupVexFlow()
-    
-    let noteString = ''
-    let timeCalculator = 0
-    vue.staves.forEach((stave,i) => {
-        if(i%vue.measuresPerLine === 0) { 
 
-            x = 0; y = 80*Math.ceil((i+1)/5)
-            
-           
+    let noteString = ''
+    vue.staves.forEach((stave, i) => {
+        if (i % vue.measuresPerLine === 0) {
+            x = 0; y = 80 * Math.ceil((i + 1) / 5)
         }
         system = makeSystem(vue.staveWidth)
         stave.notes.forEach(note => {
-            timeCalculator+=note.duration
-            noteString+= note.string + ','
+            noteString += note.string + ','
         })
-        let notes = score.notes(noteString)  
+        let notes = score.notes(noteString)
         let voice = score.voice(notes)
         let voiceList = [voice]
-        let RenderedStave = {voices: voiceList}
-        
+        let RenderedStave = { voices: voiceList }
 
-        if(i%vue.measuresPerLine === 0) {
+
+        if (i % vue.measuresPerLine === 0) {
             system.addStave(RenderedStave).addClef(vue.clef).addTimeSignature(vue.timeSignature);
         }
-        else {system.addStave(RenderedStave)}
-        
+        else { system.addStave(RenderedStave) }
+
         vf.draw();
-       
-        
         noteString = ''
         timeCalculator = 0
 
     })
-    
+
 }
 
 var x = 0;
@@ -76,7 +138,9 @@ function makeSystem(width) {
     var system = vf.System({ x: x, y: y, width: width, spaceBetweenStaves: 10 });
     x += width;
     return system;
-  }
+}
+
+
 
 
 
